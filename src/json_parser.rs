54c10c3fd67880f2edbea1fb8d0787c78efd3459
Paul Fesenmeier <pfesenmeier@gmail.com>
2022-01-09 18:02:07 -0500

    make clippy happy

--- src/json_parser.rs
+++ src/json_parser.rs
@@ -1,9 +1,5 @@
-extern crate heck;
-extern crate serde_derive;
-extern crate serde_json;
-
-use heck::{CamelCase};
-use serde_json::{Value};
+use heck::CamelCase;
+use serde_json::Value;
 
 #[cfg(test)]
 mod tests {
@@ -23,22 +19,22 @@ pub fn set_pub(public: String) {
     }
 }
 
-
 pub fn set_derive(derive: String) {
     unsafe {
         DERIVE = derive;
     }
 }
 
-pub fn set_camel(camel:String) {
-    unsafe {
-        CAMEL = camel
-    }
+pub fn set_camel(camel: String) {
+    unsafe { CAMEL = camel }
 }
 
-pub fn rust_parse(params: &Value, struct_name: &String) -> String {
+pub fn rust_parse(params: &Value, struct_name: &str) -> String {
     unsafe {
-        let struct_header = format!("{}\n{}\n{} struct {} {}", CAMEL, DERIVE, PUB, struct_name, "{");
+        let struct_header = format!(
+            "{}\n{}\n{} struct {} {}",
+            CAMEL, DERIVE, PUB, struct_name, "{"
+        );
         let mut fields: Vec<String> = vec![];
         let mut new_struct = String::new();
         if params.is_object() {
@@ -47,10 +43,15 @@ pub fn rust_parse(params: &Value, struct_name: &String) -> String {
             fields.sort();
             new_struct = cur_res.1;
         }
-        let res = format!("{}\n{}\n{}\n{}", struct_header, fields.join("\n"), "}\n", new_struct);
-        return res;
+        let res = format!(
+            "{}\n{}\n{}\n{}",
+            struct_header,
+            fields.join("\n"),
+            "}\n",
+            new_struct
+        );
+        res
     }
-
 }
 
 /// 对map类型的值进行处理
@@ -73,30 +74,28 @@ fn is_object(params: &Value) -> (Vec<String>, String) {
             }
         } else if val.is_array() {
             let cur = val.as_array().unwrap();
-            if cur.len() > 0 {
+            if !cur.is_empty() {
                 let cur_val = is_array(val);
                 println!("{:?}", &came_key);
                 cur_struct = is_ok(&cur_type, &came_key, cur_val, ok)
             }
             println!("{:?}", &cur_struct);
         }
-        new_struct = new_struct + cur_struct.as_str();
+        new_struct += cur_struct.as_str();
         unsafe {
             let field = format!("    {} {}: {},", PUB, snake_key, cur_type);
             fields.push(field);
         }
     }
-    return (fields, new_struct);
+    (fields, new_struct)
 }
 
-fn is_ok(cur_type: &String, came_key: &String, val: &Value, ok: bool) -> String {
+fn is_ok(cur_type: &str, came_key: &str, val: &Value, ok: bool) -> String {
     if ok {
-        let next_key = cur_type.as_str().to_camel_case();
-        let cur_struct = rust_parse(val, &next_key);
-        return cur_struct;
+        let next_key = cur_type.to_camel_case();
+        rust_parse(val, &next_key)
     } else {
-        let cur_struct = rust_parse(val, came_key);
-        return cur_struct;
+        rust_parse(val, came_key)
     }
 }
 
@@ -104,11 +103,11 @@ fn is_ok(cur_type: &String, came_key: &String, val: &Value, ok: bool) -> String 
 fn is_array(params: &Value) -> &Value {
     let cur = params.as_array().unwrap();
     let val = cur.get(0).unwrap();
-    return val;
+    val
 }
 
 /// 获取数据类型
-fn get_data_type(params: &Value, key: &String) -> (String, bool, bool) {
+fn get_data_type(params: &Value, key: &str) -> (String, bool, bool) {
     let mut ok = false;
     let mut flag = true;
     if params.is_object() {
@@ -116,22 +115,22 @@ fn get_data_type(params: &Value, key: &String) -> (String, bool, bool) {
         let res = key_exists(cur_key.clone(), cur_key.clone());
         cur_key = res.0;
         ok = res.1;
-        let mut cur_type = format!("{}", cur_key.as_str().to_camel_case());
+        let mut cur_type = cur_key.as_str().to_camel_case();
         let flag_str = serde_json::to_string(params).unwrap();
         if flag_str == "{}" {
             cur_type = String::from("HashMap<String, Value>");
             flag = false
         }
-        return (cur_type, ok, flag);
+        (cur_type, ok, flag)
     } else if params.is_string() {
         let cur_type = String::from("String");
-        return (cur_type, ok, flag);
+        (cur_type, ok, flag)
     } else if params.is_i64() {
         let cur_type = String::from("i64");
-        return (cur_type, ok, flag);
+        (cur_type, ok, flag)
     } else if params.is_boolean() {
         let cur_type = String::from("bool");
-        return (cur_type, ok, flag);
+        (cur_type, ok, flag)
     } else if params.is_array() {
         let values = params.as_array().unwrap();
         let first = values.get(0).unwrap_or(&serde_json::Value::Null);
@@ -143,23 +142,23 @@ fn get_data_type(params: &Value, key: &String) -> (String, bool, bool) {
         ok = cur.1;
         flag = cur.2;
         let cur_type = format!("Vec<{}>", cur.0);
-        return (cur_type, ok, flag);
+        (cur_type, ok, flag)
     } else if params.is_f64() {
         let cur_type = String::from("f64");
-        return (cur_type, ok, flag);
+        (cur_type, ok, flag)
     } else if params.is_u64() {
         let cur_type = String::from("u64");
-        return (cur_type, ok, flag);
+        (cur_type, ok, flag)
     } else {
         let cur_type = String::from("Value");
-        return (cur_type, ok, flag);
+        (cur_type, ok, flag)
     }
 }
 
 fn key_exists(key: String, mut new_key: String) -> (String, bool) {
     let mut ok = false;
     unsafe {
-        INDEX = INDEX + 1;
+        INDEX += 1;
         let cur_key = format!("{}{}", key, INDEX);
         if STRUCT_NAME.contains(&new_key) {
             ok = true;
@@ -170,5 +169,5 @@ fn key_exists(key: String, mut new_key: String) -> (String, bool) {
         }
         INDEX = 0;
     }
-    return (new_key, ok);
+    (new_key, ok)
 }
